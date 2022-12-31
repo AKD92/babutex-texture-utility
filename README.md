c78504376d37e0f07c8599d9cc8ab054eda58c1b
Ashis Kumar Das <akd.bracu@gmail.com>
2022-12-31 20:56:53 +0600

    UPDATE update function, banner moved to assets.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BABUTEX
 An utility program useful for working with the game textures of [GTA III](https://gta.fandom.com/wiki/Grand_Theft_Auto_III), [GTA Vice City](https://gta.fandom.com/wiki/Grand_Theft_Auto:_Vice_City) and [GTA San Andreas](https://gta.fandom.com/wiki/Grand_Theft_Auto:_San_Andreas).
 
-<img align="center" src="https://github.com/AKD92/babutex-texture-utility/raw/main/banner.png">
+<img align="center" src="https://github.com/AKD92/babutex-texture-utility/raw/main/assets/banner.png">
 
 ## Possible Release Date
 This software is currently on the development phase. The first working version will be released within **July 30th, 2021**. The readme file will be updated as well, reflecting the usage and purpose of this program and the problems it will solve.
--- src/btex_update.c
+++ src/btex_update.c
@@ -40,29 +40,32 @@ int compare_texture_by_filename(const void *data1, const void *data2) {
 
 
 
-int btex_update(const char *texture_root_dir, const char *src_dir) {
+int btex_update(const char *dest_root_dir, const char *src_dir) {
     
     int opval, key_pressed;
     int retval, isempty, isnull;
-    List directories, textures, src_textures;
+    List dest_dirs, dest_textures, src_textures;
     ListElem *dir_elem, *tex_elem, *src_tex_elem;
-    char *directory;
+    char *dest_dir;
     struct BTEX_TEXTURE *texture;
-    char directory_full_path[2048];
+    char buffer[2048];
+    char src_full_path[2048], *dest_full_path[2048];
+    struct BTEX_TEXTURE *src_texture, *dest_texture;
     
-    isnull = texture_root_dir == NULL || src_dir == NULL;
-    isempty = strlen(texture_root_dir) == 0 || strlen(src_dir) == 0;
+    isnull = dest_root_dir == NULL || src_dir == NULL;
+    isempty = strlen(dest_root_dir) == 0 || strlen(src_dir) == 0;
     
     if (isnull || isempty) {
         return -1;
     }
     
     // Initialize linked lists which will hold subdirectories of texture root dir
-    (void) list_init(&directories, free);
+    // and the textures on the source directory
+    (void) list_init(&dest_dirs, free);
     (void) list_init(&src_textures, free_btex_texture);
     
     // Get the list of subdirectories of the texture root directory
-    opval = btex_list_directories(texture_root_dir, &directories);
+    opval = btex_list_directories(dest_root_dir, &dest_dirs);
     if (opval == -2) {
         printf("Could not find root directory.\n");
         retval = -2;
@@ -78,18 +81,18 @@ int btex_update(const char *texture_root_dir, const char *src_dir) {
     }
     
     // Iterate over each subdirectory on the root folder
-    dir_elem = list_head(&directories);
+    dir_elem = list_head(&dest_dirs);
     while (dir_elem != NULL) {
         
         // Read the current subdirectory
-        directory = (char *) list_data(dir_elem);
+        dest_dir = (char *) list_data(dir_elem);
         
         // Initialize linked list which will hold all the textures
         // which belong to the current subdirectory
-        (void) list_init(&textures, free_btex_texture);
+        (void) list_init(&dest_textures, free_btex_texture);
         
         // Read all the textures in this subdirectory
-        opval = btex_list_textures((const char *) texture_root_dir, (const char *) directory, &textures);
+        opval = btex_list_textures((const char *) dest_root_dir, (const char *) dest_dir, &dest_textures);
         if (opval != 0) {
             // Error
         }
@@ -98,20 +101,28 @@ int btex_update(const char *texture_root_dir, const char *src_dir) {
             // Try each source texture and see if it is on the destination dir by filename
             src_tex_elem = list_head(&src_textures);
             while (src_tex_elem != NULL) {
-                opval = list_search(&textures, list_data(src_tex_elem), &tex_elem, compare_texture_by_filename);
+                opval = list_search(&dest_textures, list_data(src_tex_elem), &tex_elem, compare_texture_by_filename);
                 
                 // If the source texture matches a destination texture by filename
                 // Then we copy it to the destination folder and replace the old one with source
                 if (opval == 1) {
+                    src_texture = (struct BTEX_TEXTURE *) list_data(src_tex_elem);
+                    dest_texture = (struct BTEX_TEXTURE *) list_data(tex_elem);
+                    (void) sprintf(&src_full_path, "%s\\%s", src_texture->file_path, src_texture->file_name);
+                    (void) sprintf(&dest_full_path, "%s\\%s",dest_texture->file_path, dest_texture->file_name);
                     
+                    // Copy source file data to destination texture
+                    (void) btex_copy((const char *) src_full_path, (const char *) dest_full_path);
                 }
+                src_tex_elem = list_next(src_tex_elem);
             }
         }
-        list_destroy(&textures);
+        (void) list_destroy(&dest_textures);
+        dir_elem = list_next(dir_elem);
     }
     
     CLEANUP:
-    list_destroy(&directories);
-    list_destroy(&src_textures);
+    (void) list_destroy(&dest_dirs);
+    (void) list_destroy(&src_textures);
     return retval;
 }
